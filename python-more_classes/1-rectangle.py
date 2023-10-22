cf1497010d8cc6b80a0baa334993e98da53e1344
Hunter2605 <108022016+Hunter2605@users.noreply.github.com>
2023-10-22 00:17:13 -0500

    A class that define parameters of a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Este módulo define la clase Rectangle."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Esta clase expone un Rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """Método constructor, tiene 2 parámetros opcionales.
 
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width (int): El ancho del Rectangle.
+            height (int): El alto del Rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Este método retorna el ancho de nuestro Rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """es una condicional para la entrada de width"""
+        """Este propiedad configura el ancho"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -31,12 +31,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """Este método retorna el alto de  nuestro Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Es una condicional para la entrada de height"""
+        """Esta propiedad configura el alto"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
