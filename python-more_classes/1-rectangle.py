95574b7a89d7348e62406e83e2e04bcf930734a1
Hunter2605 <108022016+Hunter2605@users.noreply.github.com>
2023-10-22 00:06:19 -0500

    a class that contains a rectangle parameters

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+class Rectangle:
+    """Esta es una clase que define un rectangulo"""
+    
+    def __init__(self, width=0, height=0):
+        """Esto define el metodo constructor"""
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        """este metodo es para retornar el valor a width""" 
+        return(self.__width)
+    
+    @width.setter
+    def width(self, value):
+        """este metodo define condicionales para la entrada width"""
+        
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Eeste metodo retorna el valor a un atributo privado heifht"""
+        return(self.__height)
+    
+    @height.setter
+    def height(self,value):
+        """Este metodo define las condiciones de entrada para height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        
+        self.__height = value
