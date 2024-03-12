dc004c49f692036598f7eace9b0e627d70a27fd1
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-03-12 23:37:12 +0000

    I update the file called 9-full_Creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,11 @@
---crear una tabla con datos.
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO `second_table` (id, name, score) VALUES (1, "John", 10);
-INSERT INTO `second_table` (id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (id, name, score) VALUES (4, "George", 8);
+--script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256),
+	score INT
+	);
+--se a√ade datos a la tabla
+INSERT INTO second_table (name, score) VALUES ("John", 10),
+("Alex", 3),
+("Bob", 14),
+("George",8);
