b913468f000266ec941df798133c35083c4642c9
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-03-20 23:03:51 +0000

    I created a new script that creates a database and a new table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
 -- Create a database and a table inside of the new database.
 CREATE DATABASE IF NOT EXISTS hbtn_od_usa;
+
 USE hbtn_od_usa;
+
 CREATE TABLE IF NOT EXISTS states(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+	name VARCHAR(256) NOT NULL
 );
