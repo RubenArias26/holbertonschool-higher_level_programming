ad8ba8f76c3e817781dd09ac0b6c0c36fbe41c71
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-03-22 17:52:56 +0000

    I created a new script that list some information of states table

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- lists all cities from california withotut using JOIN keyword.
+SELECT *
+FROM cities
+WHERE state_id =
+	( SELECT id
+	FROM states
+	WHERE name = "California"
+	)
+ORDER BY id ASC;
