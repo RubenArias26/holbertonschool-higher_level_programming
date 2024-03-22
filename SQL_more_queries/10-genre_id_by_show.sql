f92fdbf0dec249e4314f641f8416a18ad51502b5
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-03-22 23:58:43 +0000

    I added a new script, that join two tables

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- list data from two diferent tables.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-INNER JOIN tv_shows
+JOIN tv_shows
 ON tv_show_genres.genre_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
