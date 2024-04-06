bb4c653c701ec86b0ffc03be1d9d5fb3ecf8cbca
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-04-06 22:40:40 +0000

    I added a new script that count the total of genders from tv shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genre.id
+GROUP BY tv_genre.name
 ORDER BY COUNT(tv_show_genres.show_id) DESC;
