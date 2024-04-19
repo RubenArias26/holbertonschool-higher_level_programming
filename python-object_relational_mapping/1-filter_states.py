f6ef36b5a8d8ca7d51e2392d2d4df94bdc7acc86
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-04-18 19:43:04 -0500

    I added a new py script that filter a query

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+COnecta con mysql
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # establece coneccion con mysql
+    db = MySQLdb.connect(
+        host = "localhost",
+        user = sys.argv[1],
+        password = sys.argv[2],
+        port = 3306,
+        database = sys.argv[3],
+    )
+
+    # establecemos un cursor
+    cur = db.cursor()
+    # creamos la consulta
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # recuperamos los datos de la consulta
+    rows = cur.fetchall()
+
+    # imprimimos la consulta
+    for row in rows:
+        print(row)
+
+    # cerramos el cursor y el connect
+    cur.close
+    db.close
