be3ef60b70668d7b6feed133a68659dba2993b92
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-04-18 19:02:28 -0500

    I added a new python script that select all states in the db hbtn

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+this module connect with mysql
+"""
+
 import MySQLdb
 import sys
 if __name__ == "__main__":
@@ -11,12 +16,16 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
+    # this is a query for obtain data order by id
     cur.execute("SELECT * FROM states ORDER BY states.id")
 
+    # retrive all the rows
     rows = cur.fetchall()
 
+    # show the result of the query
     for row in rows:
         print(row)
 
+    # close the cursor and connection
     cur.close
     db.close
