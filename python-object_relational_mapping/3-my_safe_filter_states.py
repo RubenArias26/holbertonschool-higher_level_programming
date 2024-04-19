9b3e0d6ad154ebd40993cc0884fd9b571beadf21
RubenArias26 <ruben.arias.5.26@gmail.com>
2024-04-19 16:10:19 -0500

    I created a new file that use sql injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+COnecta con mysql
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # establece coneccion con mysql
+    db = MySQLdb.connect(
+        host = "localhost",
+        user = sys.argv[1],
+        password = sys.argv[2],
+        port = 3306,
+        database = sys.argv[3],
+    )
+
+    # establecemos un cursor
+    cur = db.cursor()
+    # creamos la consulta
+    match = (sys.argv[4])
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC", (match,))
+
+    # recuperamos los datos de la consulta
+    rows = cur.fetchall()
+
+    # imprimimos la consulta
+    for row in rows:
+        print(row)
+
+    # cerramos el cursor y el connect
+    cur.close
+    db.close
